e50ed3c16f0de54a63b8135131365df7696247e6
JatinVC <Jchandwaney@gmail.com>
2025-03-22 18:58:38 +0800

    reorganize functions in pieces

--- services/chess-game/scripts/pieces.js
+++ services/chess-game/scripts/pieces.js
@@ -11,6 +11,56 @@ let turnNumber = 1;
 let whichPlayer = WHITE;
 const width = 8;
 
+const nextTurn = () => {
+    if(whichPlayer === WHITE) {
+        whichPlayer = BLACK;
+    }else{
+        turnNumber++;
+        whichPlayer = WHITE;
+    }
+}
+
+const checkIfValidMove = (startPositionId, targetPositionId) => {
+    return initialBoard[startPositionId].isValidMove(startPositionId, targetPositionId);
+}
+
+/**
+ * Reverses the board for the white player, since the first 16 pieces are black and the last 16 pieces are white.
+ */
+const reverseBoard = () => {
+    const allSquares = document.querySelectorAll('.square');
+    allSquares.forEach((square, id) =>
+        square.setAttribute('square-id', (width * width - 1) - id))
+}
+
+/**
+ * Reverts the board back to its original state. where the first 16 pieces are black and the last 16 pieces are white.
+ */
+const resetBoard = () => {
+    const allSquares = document.querySelectorAll('.square');
+    allSquares.forEach((square, id) =>
+        square.setAttribute('square-id', id))
+}
+
+/**
+ * Updates the board array with the new position of the piece.
+ * @param startPositionId - the id of the square where the piece was before moving
+ * @param targetPositionId - the id of the square where the piece is moved
+ */
+const updateBoard = (startPositionId, targetPositionId) => {
+    // if its white turn then reset board turn before moving the piece
+    if(whichPlayer === WHITE){
+        resetBoard();
+    }
+
+    initialBoard[targetPositionId] = initialBoard[startPositionId];
+    initialBoard[startPositionId] = '';
+
+    if(whichPlayer === WHITE){
+        reverseBoard();
+    }
+}
+
 const dragStartHandler = (e) => {
     draggedPiece = e.target;
 }
@@ -33,12 +83,21 @@ const dragDropHandler = (e) => {
     if (isCurrentPlayerPiece) {
         const whichOpponent = whichPlayer === WHITE ? BLACK : WHITE;
         const isSquareOccupiedByOpponent = e.target.firstChild?.classList.contains(whichOpponent);
-        let valid = checkIfValidMove(e.target);
+
+        let targetSquare = e.target;
+
+        const targetPositionId = Number(targetSquare.getAttribute('square-id'))
+            || Number(targetSquare.parentNode.getAttribute('square-id'));
+
+        const startPositionId = Number(draggedPiece.parentNode.getAttribute('square-id'));
+
+        let valid = checkIfValidMove(startPositionId, targetPositionId);
 
         // If square is occupied by opponent, and move is valid, then remove the opponent piece and append the dragged piece
         if (isSquareOccupiedByOpponent && valid) {
             e.target.remove();
             e.target.parentNode.appendChild(draggedPiece);
+            updateBoard(startPositionId, targetPositionId);
             nextTurn();
             return;
         }
@@ -51,6 +110,7 @@ const dragDropHandler = (e) => {
         // if square is not occupied by any piece, then append the dragged piece
         if(valid){
             e.target.append(draggedPiece);
+            updateBoard(startPositionId, targetPositionId);
             nextTurn();
         }
     }
@@ -64,48 +124,4 @@ allSquares.forEach((square) => {
     square.addEventListener('drop', dragDropHandler);
 })
 
-const nextTurn = () => {
-    if(whichPlayer === WHITE) {
-        whichPlayer = BLACK;
-    }else{
-        turnNumber++;
-        whichPlayer = WHITE;
-    }
-}
-
-const reverseBoard = () => {
-    const allSquares = document.querySelectorAll('.square');
-    allSquares.forEach((square, id) =>
-        square.setAttribute('square-id', (width * width - 1) - id))
-}
-
-const revertBoard = () => {
-    const allSquares = document.querySelectorAll('.square');
-    allSquares.forEach((square, id) =>
-        square.setAttribute('square-id', id))
-}
-
-const checkIfValidMove = (targetSquare) => {
-    const targetPositionId = Number(targetSquare.getAttribute('square-id'))
-        || Number(targetSquare.parentNode.getAttribute('square-id'));
-
-    const startPositionId = Number(draggedPiece.parentNode.getAttribute('square-id'));
-
-    let isValidMove = initialBoard[startPositionId].isValidMove(startPositionId, targetPositionId);
-    if(isValidMove){
-
-        //if its white turn then revert turn  before changing board ids
-        if(whichPlayer === WHITE){
-            revertBoard();
-        }
-        initialBoard[targetPositionId] = initialBoard[startPositionId];
-        initialBoard[startPositionId] = '';
-        if(whichPlayer === WHITE){
-            reverseBoard();
-        }
-    }
-
-    return isValidMove;
-
-}
 export {reverseBoard};
