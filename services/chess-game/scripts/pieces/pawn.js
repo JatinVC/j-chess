3131f16c6282eccbc3c6aeda49d0697fc83367cd
JatinVC <Jchandwaney@gmail.com>
2025-03-22 18:59:41 +0800

    comments for clarity

--- services/chess-game/scripts/pieces/pawn.js
+++ services/chess-game/scripts/pieces/pawn.js
@@ -20,6 +20,11 @@ class Pawn extends Piece {
     isValidMove(startPositionId, targetPositionId){
         const startRow = this.startingPosition[this.color];
 
+        /**
+         * If the pawn is in the starting position, it can move two squares up the board.
+         * The pawn can move one square up the board.
+         * The pawn can move one square up the board diagonally if there is an opponent's piece.
+         */
         return (startRow.includes(startPositionId) && startPositionId + (this.width * 2) === targetPositionId ||
             startPositionId + this.width === targetPositionId ||
             startPositionId + this.width - 1 === targetPositionId && document.querySelector(`[square-id="${startPositionId + this.width - 1}"]`).firstChild ||
