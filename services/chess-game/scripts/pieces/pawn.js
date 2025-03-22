0c506d6f86aeab6292dc91c27074644b62083177
JatinVC <Jchandwaney@gmail.com>
2025-03-22 18:58:56 +0800

    fix pawn validation logic to work with white and black

--- services/chess-game/scripts/pieces/pawn.js
+++ services/chess-game/scripts/pieces/pawn.js
@@ -2,18 +2,28 @@ import {pawn} from "../pieces_images.js";
 import {Piece} from "./piece.js";
 
 class Pawn extends Piece {
+
+    startingPosition = {
+        'white': [48,49,50,51,52,53,54,55],
+        'black': [8,9,10,11,12,13,14,15]
+    }
+
     constructor(name, color){
         super(name, color);
 
+        // if the piece is white then it should move up the board (towards the higher square ids)
+        if (color === 'white'){
+            this.width = -this.width;
+        }
     }
 
     isValidMove(startPositionId, targetPositionId){
-        const startRow = [8,9,10,11,12,13,14,15];
-        return !!(startRow.includes(startPositionId) && startPositionId + (this.width * 2) === targetPositionId ||
+        const startRow = this.startingPosition[this.color];
+
+        return (startRow.includes(startPositionId) && startPositionId + (this.width * 2) === targetPositionId ||
             startPositionId + this.width === targetPositionId ||
             startPositionId + this.width - 1 === targetPositionId && document.querySelector(`[square-id="${startPositionId + this.width - 1}"]`).firstChild ||
             startPositionId + this.width + 1 === targetPositionId && document.querySelector(`[square-id="${startPositionId + this.width + 1}"]`).firstChild);
-
     }
 
     getPiece () {
